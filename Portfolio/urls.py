5520b0c2c1a4abb133f1a3e2c8661c6310975d8c
blackhat <you@example.com>
2022-07-23 14:18:25 +0530

    code

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from apps.build import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
